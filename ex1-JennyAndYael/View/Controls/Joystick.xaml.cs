6c3548836f9b25f96d329f0f2c48358185107c0b
JennyDavidov <48158720+JennyDavidov@users.noreply.github.com>
2020-03-22 12:32:27 +0200

    joystick- commit 1

---
+++ ex1-JennyAndYael/View/Controls/Joystick.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ex1_JennyAndYael.View.Controls
+{
+    public partial class Joystick : UserControl 
+    {
+        public Joystick()
+        {
+            InitializeComponent();
+        }
+        void centerKnob_Completed(object sender, EventArgs e){ }
+
+        private void Knob_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
