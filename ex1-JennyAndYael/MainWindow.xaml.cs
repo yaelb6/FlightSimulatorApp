d10805325956d964dab935199e4f05e17e8f2d43
Yael Berman <yael.berman2@live.biu.ac.il>
2020-03-22 11:09:02 +0200

    second commit

--- ex1-JennyAndYael/MainWindow.xaml.cs
+++ ex1-JennyAndYael/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ex1_JennyAndYael
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
