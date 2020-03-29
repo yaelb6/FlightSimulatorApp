e231d254331ef86c9aad2d5f9091d29ea1bd30b5
Yael Berman <yael.berman2@live.biu.ac.il>
2020-03-29 11:51:53 +0300

    joystick VM

---
+++ ex1-JennyAndYael/JoyStickViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ex1_JennyAndYael
+{
+    class JoyStickViewModel : INotifyPropertyChanged
+    {
+        private MyModel simulatorModel;
+        private double rudder;
+        private double elevator;
+        private double throttle;
+        private double aileron;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public JoyStickViewModel(MyModel model)
+        {
+            this.simulatorModel = model;
+        }
+        public double VM_Rudder
+        {
+            get
+            {
+                return this.rudder;
+            }
+            set
+            {
+                rudder = value;
+            }
+        }
+        public double VM_Elevator
+        {
+            get
+            {
+                return this.elevator;
+            }
+            set
+            {
+                elevator = value;
+            }
+        }
+        public double VM_Throttle
+        {
+            get
+            {
+                return this.throttle;
+            }
+            set
+            {
+                throttle = value;
+            }
+        }
+        public double VM_Aileron
+        {
+            get
+            {
+                return this.aileron;
+            }
+            set
+            {
+                aileron = value;
+            }
+        }
+    }
+}
