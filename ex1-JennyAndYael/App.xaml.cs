d2c0b95823f54fb5e2360be0ceaff0cffb514c05
JennyDavidov <48158720+JennyDavidov@users.noreply.github.com>
2020-04-03 22:18:18 +0300

    App

--- ex1-JennyAndYael/App.xaml.cs
+++ ex1-JennyAndYael/App.xaml.cs
@@ -13,5 +13,10 @@ namespace ex1_JennyAndYael
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
+        }
     }
 }
